4b4f0bea53c982a3aa65f1c0af735f7014cf5460
Henrique <henriqbeltrame@gmail.com>
2020-10-17 15:01:54 -0300

    Primeiro commit

---
+++ src/main/java/com/empresa/cursospringsecurity/CursoSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.cursospringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringSecurityApplication.class, args);
+	}
+
+}
